39b95645eca1ac8b80c8152758c8fd98f72f9314
Tokinantenaina <tokib2oba@yahoo.com>
2024-05-15 12:27:21 +0300

    commit back

---
+++ controllers/auth.controller.js
@@ -0,0 +1,37 @@
+const UserModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+const { signUpErr, signInErr } = require("../utils/errors.utils");
+const SECRET_TOKEN = process.env.SECRET_TOKEN;
+const createToken = id => {
+  return jwt.sign({ id }, SECRET_TOKEN);
+};
+
+module.exports.signUp = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const newUser = await UserModel.create({ username, email, password });
+    res.status(201).json({ user: newUser._id });
+  } catch (err) {
+    const errors = signUpErr(err);
+    res.status(500).send({ err });
+  }
+};
+
+module.exports.signIn = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    const errors = signInErr(err);
+    res.status(401).json({ errors });
+  }
+};
+
+module.exports.logout = async (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
---
+++ server.js
@@ -0,0 +1,63 @@
+require("dotenv").config({ path: "./config/.env" });
+require("./config/dbConnex");
+//
+const express = require("express");
+const app = express();
+//
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+const upload_checking_error = require("./middleware/uploadError.middleware");
+//
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(",");
+
+console.log(allowedOrigins);
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
+  preflightContinue: false
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//jwt
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  if (res.locals.user) {
+    res.status(200).send(res.locals.user._id);
+  } else {
+    res.status(401).json({ message: "Utilisateur non authentifié" });
+  }
+});
+
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/post", postRoutes);
+
+//middleware upload error
+app.use("*/upload", upload_checking_error);
+
+//server
+app.listen(PORT, () => {
+  console.log("listen on " + PORT);
+});
