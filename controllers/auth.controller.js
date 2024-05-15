382bfd2f492867bc1e8e18e04f99e01453cda9e5
Tokinantenaina <tokib2oba@yahoo.com>
2024-05-15 19:50:01 +0300

    update signIn

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -23,7 +23,12 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge, secure: true });
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge,
+      secure: true,
+      sameSite: "Strict"
+    });
     res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = signInErr(err);
